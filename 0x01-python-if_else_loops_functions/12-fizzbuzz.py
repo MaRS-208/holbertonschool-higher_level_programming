c0d4ccf849dd6e36c75a6c0eee97e8e0a72c68a4
MaRS-208 <manuela.riossosa@gmail.com>
2022-04-26 15:19:33 -0700

    12. Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for fb in range(101):
+        if fb % 3 == 0 and fb % 5 == 0:
+            print(f'FizzBuzz ', end='')
+            continue
+        elif fb % 3 == 0:
+            print(f'Fizz ', end='')
+            continue
+        elif fb % 5 == 0:
+            print(f'Buzz ', end='')
+            continue
+        print(f'{fb} ', end='')
