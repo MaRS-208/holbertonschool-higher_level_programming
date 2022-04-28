22ff1775aab7346c823b557d8c2ae7c4a8137684
MaRS-208 <manuela.riossosa@gmail.com>
2022-04-28 12:07:52 -0700

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+
+    i = 0 
+    for indx in range(1, argc):
+        i += int(argv[indx])
+    print(i)
