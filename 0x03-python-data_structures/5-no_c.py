597951601b8bfc869625e383c250135d8dd66e0d
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-02 11:07:05 -0700

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_list = []
+    for x in my_string:
+        if x != "c" and x != "C":
+            my_list.append(x)
+    return "".join(my_list)
+        
