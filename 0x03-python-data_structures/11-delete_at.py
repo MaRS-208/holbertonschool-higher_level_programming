43956474c2c17bc7c4fd51dc87fc84fa910b486d
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-03 13:04:29 -0700

    11. Delete atII

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    b4 = list(my_list[0 : idx])
+    b4 = list(my_list[0: idx])
     aftr = list(my_list[idx + 1:])
     new = b4 + aftr
     for idx in range(len(new)):
