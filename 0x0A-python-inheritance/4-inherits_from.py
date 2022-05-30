9364e4b7f9b610b412210bcde77afc8a13e95330
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 14:35:09 -0700

    4. Only sub class ofII

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ from the specified class ; otherwise False """
 
 def inherits_from(obj, a_class):
     """ returns True o False """
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    if type(obj), a_class) and type(obj) != a_class:
+        return True
