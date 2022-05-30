cb956626f96456c0af08fe96efe49299afe558d6
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 12:03:00 -0700

    0. LookupII

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-    module
+    lookup
 """
 
 
 def lookup(obj):
     """ returns the list of available attributes and methods of an object """
     if type(obj):
-        return dir(obj)
+        return list(dir(obj))
