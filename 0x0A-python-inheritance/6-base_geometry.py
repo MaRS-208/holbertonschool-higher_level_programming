9a095e757a6f12eaac2151974edec8836dcda9d5
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 15:44:33 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+
+
+class BaseGeometry():
+    """ class """
+
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemeented")
