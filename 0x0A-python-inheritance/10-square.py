801f4df186cb27f5b8f3b4b174a496bc26197834
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 19:13:19 -0700

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """init"""
+    def __init__(self, size):
+        """initializing"""
+        self.__size = self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
