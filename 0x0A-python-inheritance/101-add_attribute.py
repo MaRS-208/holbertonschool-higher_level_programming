5a7471a981beba0ee5a0bcb8b744a1d6052e3b9b
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 19:56:08 -0700

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,4 +3,8 @@
     module
 """
 
-ijvbvj,v
+class def add_attribute(a_class, a_name, a_value):
+    """ adds attributes """
+    if not hasattr(a_class, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(a_class, a_name, a_value)
