57a9a54a4d0d6ef29f7bb20df4ee8446c95ac1d1
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 19:42:42 -0700

    12. My integerII

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """my int"""
     def __eq__(self, other):
         """eq"""
-        return self != other
+        return self.real != other.real
     def__ne__(self, other):
         """ne"""
         return not self.__eq__(other)
