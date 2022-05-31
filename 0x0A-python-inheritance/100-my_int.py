042ee4e4cef0b9399ebd5f9c1983515745765890
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-30 20:16:39 -0700

    12. My integerV

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,9 +4,11 @@
 
 class MyInt(int):
     """my int"""
+
     def __eq__(self, other):
         """eq"""
         return self.real != other.real
+
     def __ne__(self, other):
         """ne"""
         return not self.__eq__(other)
