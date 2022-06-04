3c3bd143f019de43d356649804b045b8eff9aca8
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-04 16:00:15 -0700

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,3 +11,16 @@ class Rectangle:
 
     def __init__(self, width, height, x=0, y=0, id=None)
         """init"""
+        if not type(width) is int:
+            raise TypeError("width must be an integer")
+        if type(width) <= 0:
+            raise ValueError("width must be > 0")
+        if not type(height) is int:
+            raise TypeError("height must be an integer")
+        if type(height) <= 0:
+            raise ValueError("height must be > 0")
+        if x <= 0:
+            raise ValueError("x must be >= 0")
+        if y <= 0:
+            raise ValueError("y must be >= 0")
+
