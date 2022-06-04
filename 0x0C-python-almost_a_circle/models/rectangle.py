989c9f93e4590f32a7ae8af058695cb62ed70fd3
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-04 16:15:00 -0700

    2. First RectangleII

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """Write the class Rectangle that inherits from Base"""
+from models.base import Base
 
 
-class Rectangle:
+class Rectangle(Base):
     """Rectangle class"""
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
 
-    def __init__(self, width, height, x=0, y=0, id=None)
+    def __init__(self, width, height, x=0, y=0, id=None):
         """init"""
-        if not type(width) is int:
+        if type(width) != int:
             raise TypeError("width must be an integer")
-        if type(width) <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
-        if not type(height) is int:
+        if type(height) != int:
             raise TypeError("height must be an integer")
-        if type(height) <= 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
-        if x <= 0:
+        if x < 0:
             raise ValueError("x must be >= 0")
-        if y <= 0:
+        if y < 0:
             raise ValueError("y must be >= 0")
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
+        super().__init__(id)
