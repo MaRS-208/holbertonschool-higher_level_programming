6333c7ca9275d5c30a7e2c7d3c957b9ea7c19128
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-04 15:36:25 -0700

    2. First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write the class Rectangle that inherits from Base"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+
+    def __init__(self, width, height, x=0, y=0, id=None)
+        """init"""
