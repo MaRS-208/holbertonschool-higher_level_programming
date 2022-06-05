f679a82fae76b95afc3b05f2a5fc5c808579006e
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-05 15:48:28 -0300

    10. And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write the class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """str representation"""
+        s1 = f"[Square] ({self.id}) "
+        s2 = f"{self.x}/{self.y} - {self.size}"
+        return s1 + s2
+
