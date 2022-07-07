1ed7fe4c1dac82ebe86ff2904545cec7a92e899d
MaRS-208 <MaRS-208>
2022-07-07 11:18:19 -0700

    5. Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Write a script that creates the table unique_id on your MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (id INT(1) NOT NULL UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (id INT(1) UNIQUE, name VARCHAR(256) NOT NULL);
