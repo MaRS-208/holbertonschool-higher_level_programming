5a0287481105ec9776d1d9957d1e65e9b62ea0a2
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-08 18:40:40 -0300

    16. Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT score, number FROM second_table WHERE score, number NOT IN (SELECT DISTINCT score, number FROM second_table WHERE name="YES");
+SELECT score, number FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
