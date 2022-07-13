bd0baf7aa9d1c8e25d3af98ab720812ef8db0075
MaRS-208 <MaRS-208>
2022-07-13 16:06:07 -0700

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,5 @@
 -- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY genre ORDER BY number_of_shows DESC;
