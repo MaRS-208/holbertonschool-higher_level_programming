5cc47bcfcb53de14b952dacc038f6078bca0120b
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-02 21:24:52 -0300

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const data = require('./100-data').list;
 
-const dt= data.map((element, index) => element * index);
+const dt = data.map((element, index) => element * index);
 console.log(data);
 console.log(dt);
