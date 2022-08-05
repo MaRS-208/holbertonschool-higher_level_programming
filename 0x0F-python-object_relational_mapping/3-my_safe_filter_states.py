6e129c01287ac273cc91010b3dd22da14ba2b02e
MaRS-208 <MaRS-208>
2022-08-05 10:46:46 -0700

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument, that is safe from MySQL injections"""
 
 if __name__ == "__main__":
     import MySQLdb
