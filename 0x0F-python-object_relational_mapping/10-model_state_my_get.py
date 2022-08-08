e1c98e70e38c22725513e4b03e83ad5b06c56d40
MaRS-208 <MaRS-208>
2022-08-08 13:46:21 -0700

    10. Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a script that lists all State objects that\
+contain the letter a from the database hbtn_0e_6_usa"""
+
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from model_state import Base
+from model_state import State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:\
+    {argv[2]}@localhost/{argv[3]}')
+    with Session(bind=engine) as session:
+        q = session.query(State).filter(State.name == argv[4]).first()
+        if q:
+            print(q.id)
+        else:
+            print('Not found')
