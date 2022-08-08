54831aab213f8baa7c8fd16e94fc9129979d6189
MaRS-208 <MaRS-208>
2022-08-08 12:08:34 -0700

    9. Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a script that lists all State objects that\
+contain the letter a from the database hbtn_0e_6_usa"""
+
+from operator import itemgetter
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from model_state import Base
+from model_state import State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:\
+    {argv[2]}@localhost/{argv[3]}')
+    with Session(bind=engine) as session:
+        q = session.query(State).filter(State.name.contains('a')).all()
+        for i in q:
+            print(f'{i.id}: {i.name}')
