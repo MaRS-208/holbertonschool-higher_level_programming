5d20be887ce06ec689a09c4c5d91011dc641e592
MaRS-208 <MaRS-208>
2022-08-08 09:19:23 -0700

    7. All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects from the database hbtn_0e_6_usa"""
+"""Write a script that lists all State\
+objects from the database hbtn_0e_6_usa"""
 
 
 from sqlalchemy.orm import Session
@@ -9,7 +10,8 @@ from model_state import State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:\
+    {argv[2]}@localhost/{argv[3]}')
     with Session(bind=engine) as session:
         q = session.query(State).all()
         for i in q:
