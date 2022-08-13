43ac73393d5f131c3328412f6519de9cee43478b
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-12 21:20:39 -0300

    12. Update a stateII

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,13 +3,13 @@
 State object from the database hbtn_0e_6_usa"""
 
 
-from sqlalchemy.orm import Session
-from sqlalchemy import (create_engine)
-from model_state import Base
-from model_state import State
-from sys import argv
-
 if __name__ == "__main__":
+    from sqlalchemy.orm import Session
+    from sqlalchemy import (create_engine)
+    from model_state import Base
+    from model_state import State
+    from sys import argv
+
     engine = create_engine(f'mysql+mysqldb://{argv[1]}:\
     {argv[2]}@localhost/{argv[3]}', pool_pre_ping=True)
 
