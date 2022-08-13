af87f5886eee12168ce548e52c059587b35053b9
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-12 21:58:59 -0300

    13. Delete statesXI

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
