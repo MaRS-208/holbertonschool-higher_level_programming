6419c6e891af707d4c8bdadc1f134e301a7bc4f7
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-12 21:33:56 -0300

    13. Delete statesII

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     Base.metadata.create_all(engine)
 
-    fsession = Session(engine)
+    session = Session(engine)
     for state in session.query(State).filter(\
         State.name.contains('a')):
         session.delete(state)
