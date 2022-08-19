fdde3a9415d8654a051d5da128cdac5a723e7965
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-19 02:36:15 +0000

    3. cURL only methodsII

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in URL and displays all HTTP methods the server will accept
-curl -sI "$1" | awk '/Allow/ { $1=""; print }'
+curl -sI "$1" | awk '/Allow/ { $1=""; sub(/^[ \t]+/, ""); print }'
