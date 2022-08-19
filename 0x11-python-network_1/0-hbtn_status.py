49230e5ef9587328d599c27f649cc3c273f7b830
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-19 20:30:46 -0300

    0. What's my status? #0III

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
         print("""Body response:
-            - type: {}
-            - content: {}
-            - utf8 content: {}""".format(
-                (type(r)), (r), (r.decode('utf-8'))))
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(
+            (type(r)), (r), (r.decode('utf-8'))))
