3087f219454b0bbc86c90f8ebab8164822954d91
MaRS-208 <manuela.riossosa@gmail.com>
2022-09-13 08:35:03 -0700

    0. ReadmeII

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  }
-  else {
-    console.log(data)
+  } else {
+    console.log(data);
   }
 });
